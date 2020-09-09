615a503f5a48e091394bf6a0e5fdc8eb4df50835
ebresafegaga <ebresafegaga@outlook.com>
2020-09-09 17:24:15 +0100

    got language serve4r working!

--- Main.hs
+++ Main.hs
@@ -1,7 +1,18 @@
+{-# LANGUAGE FunctionalDependencies #-}
+
+
 module Main where
 
 --f :: [Int] -> [Bool] -> Int 
 f is bs = length is + length bs
+
+class Mutation m r | m -> r where 
+    newRef :: a -> m (r a) 
+    readRef :: r a -> m a
+    writeRef :: r a -> a -> m ()
+
+class Mutation' m where 
+    type Ref m :: * -> *
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
